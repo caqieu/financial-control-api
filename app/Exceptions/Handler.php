1223c1be0e1ac9bd3574a094f3ce44f7e3bdd224
CaiqueSallas <caique.sallas2002@hotmail.com>
2022-08-06 00:17:47 -0300

    chore: database, migrations and config files

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -2,7 +2,19 @@
 
 namespace App\Exceptions;
 
+use App\Helpers\ApiResponse;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Database\QueryException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use Illuminate\Validation\ValidationException;
+use Laravel\Passport\Exceptions\OAuthServerException as PassportOAuthServerException;
+use League\OAuth2\Server\Exception\OAuthServerException;
+use Psr\Log\LogLevel;
+use Symfony\Component\HttpKernel\Exception\HttpException;
+use Symfony\Component\HttpKernel\Exception\MethodNotAllowedHttpException;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Throwable;
 
 class Handler extends ExceptionHandler
@@ -10,7 +22,7 @@ class Handler extends ExceptionHandler
     /**
      * A list of exception types with their corresponding custom log levels.
      *
-     * @var array<class-string<\Throwable>, \Psr\Log\LogLevel::*>
+     * @var array<class-string<Throwable>, LogLevel::*>
      */
     protected $levels = [
         //
@@ -19,7 +31,7 @@ class Handler extends ExceptionHandler
     /**
      * A list of the exception types that are not reported.
      *
-     * @var array<int, class-string<\Throwable>>
+     * @var array<int, class-string<Throwable>>
      */
     protected $dontReport = [
         //
@@ -46,5 +58,59 @@ class Handler extends ExceptionHandler
         $this->reportable(function (Throwable $e) {
             //
         });
+    }
+
+    /**
+     * Render an exception into an HTTP response.
+     *
+     * @param Request $request
+     * @param Throwable $exception
+     * @return JsonResponse
+     *
+     * @throws Throwable
+     */
+    public function render(
+        $request,
+        Throwable $exception
+    ): JsonResponse {
+        $data = null;
+        $code = $exception->getCode();
+        $message = $exception->getMessage();
+
+        if ($exception instanceof HttpException) {
+            $code = $exception->getStatusCode();
+        }
+
+        if ($exception instanceof ValidationException) {
+            $code = 401;
+            $data = $exception->errors();
+            $message = 'Dados Inválidos';
+        }
+
+        if ($exception instanceof NotFoundHttpException && empty($message)) {
+            $code = 404;
+            $message = 'Não encontrado';
+        }
+
+        if (!is_numeric($code) || $code < 200 || $code > 500) {
+            $code = 500;
+            $data = $exception->getTrace();
+
+            if (!env('APP_DEBUG')) {
+                $data = null;
+                $message = 'Erro interno no servidor';
+            }
+        }
+
+        $response = [
+            'error' => true,
+            'message' => $message
+        ];
+
+        if ($data != null) {
+            $response = array_merge($response, ['data' => $data]);
+        }
+
+        return response()->json($response, $code);
     }
 }
